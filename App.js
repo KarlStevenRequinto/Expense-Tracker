7487ff78ac61e508d403b9397eb86cfbfedf6e1b
KarlStevenRequinto <lerdrobott@gmail.com>
2022-10-25 14:05:57 +0800

    added util, styling for expense items, headerRight add icon

--- App.js
+++ App.js
@@ -3,7 +3,7 @@ import { NavigationContainer } from "@react-navigation/native";
 import { createNativeStackNavigator } from "@react-navigation/native-stack";
 import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
 import { Ionicons } from "@expo/vector-icons";
-
+import IconButton from "./UI/IconButton";
 import ManageExpense from "./screens/ManageExpense";
 import RecentExpenses from "./screens/RecentExpenses";
 import AllExpenses from "./screens/AllExpenses";
@@ -13,55 +13,84 @@ const Stack = createNativeStackNavigator();
 const BottomTabs = createBottomTabNavigator();
 
 function ExpensesOverview() {
-  return (
-    <BottomTabs.Navigator
-      screenOptions={{
-        headerStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-        headerTintColor: "white",
-        tabBarStyle: { backgroundColor: GlobalStyles.colors.primary500 },
-        tabBarActiveTintColor: GlobalStyles.colors.accent500,
-      }}
-    >
-      <BottomTabs.Screen
-        name="RecentExpenses"
-        component={RecentExpenses}
-        options={{
-          title: "Recent Expenses",
-          tabBarLabel: "Recent",
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="hourglass" size={size} color={color} />;
-          },
-        }}
-      />
-      <BottomTabs.Screen
-        name="AllExpenses"
-        component={AllExpenses}
-        options={{
-          title: "All Expenses",
-          tabBarLabel: "All Expenses",
-          tabBarIcon: ({ color, size }) => {
-            return <Ionicons name="calendar" size={size} color={color} />;
-          },
-        }}
-      />
-    </BottomTabs.Navigator>
-  );
+    return (
+        <BottomTabs.Navigator
+            screenOptions={{
+                headerStyle: {
+                    backgroundColor: GlobalStyles.colors.primary500,
+                },
+                headerTintColor: "white",
+                tabBarStyle: {
+                    backgroundColor: GlobalStyles.colors.primary500,
+                },
+                tabBarActiveTintColor: GlobalStyles.colors.accent500,
+                headerRight: ({ tintColor }) => {
+                    return (
+                        <IconButton
+                            icon="add"
+                            size={24}
+                            color={tintColor}
+                            onPress={() => {}}
+                        />
+                    );
+                },
+            }}
+        >
+            <BottomTabs.Screen
+                name="RecentExpenses"
+                component={RecentExpenses}
+                options={{
+                    title: "Recent Expenses",
+                    tabBarLabel: "Recent",
+                    tabBarIcon: ({ color, size }) => {
+                        return (
+                            <Ionicons
+                                name="hourglass"
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    },
+                }}
+            />
+            <BottomTabs.Screen
+                name="AllExpenses"
+                component={AllExpenses}
+                options={{
+                    title: "All Expenses",
+                    tabBarLabel: "All Expenses",
+                    tabBarIcon: ({ color, size }) => {
+                        return (
+                            <Ionicons
+                                name="calendar"
+                                size={size}
+                                color={color}
+                            />
+                        );
+                    },
+                }}
+            />
+        </BottomTabs.Navigator>
+    );
 }
 
 export default function App() {
-  return (
-    <>
-      <StatusBar style="auto" />
-      <NavigationContainer>
-        <Stack.Navigator>
-          <Stack.Screen
-            name="ExpensesOverview"
-            component={ExpensesOverview}
-            options={{ headerShown: false }}
-          />
-          <Stack.Screen name="ManageExpense" component={ManageExpense} />
-        </Stack.Navigator>
-      </NavigationContainer>
-    </>
-  );
+    return (
+        <>
+            <StatusBar style="auto" />
+            <NavigationContainer>
+                <Stack.Navigator>
+                    <Stack.Screen
+                        name="ExpensesOverview"
+                        component={ExpensesOverview}
+                        options={{ headerShown: false }}
+                    />
+                    <Stack.Screen
+                        name="ManageExpense"
+                        component={ManageExpense}
+                    />
+                </Stack.Navigator>
+            </NavigationContainer>
+        </>
+    );
 }
--- components/ExpensesOutput/ExpensesOutput.js
+++ components/ExpensesOutput/ExpensesOutput.js
@@ -26,7 +26,7 @@ const DUMMY_EXPENSES = [
         id: "e4",
         description: "A PAIR OF BRAINS",
         amount: 69.99,
-        date: new Date("2022-9-23"),
+        date: new Date("2022-09-23"),
     },
     {
         id: "e5",
@@ -46,6 +46,24 @@ const DUMMY_EXPENSES = [
         amount: 69.99,
         date: new Date("2022-11-25"),
     },
+    {
+        id: "e8",
+        description: "A PAIR OF HANDS",
+        amount: 69.99,
+        date: new Date("2022-10-21"),
+    },
+    {
+        id: "e9",
+        description: "A PAIR OF SOMETHING",
+        amount: 69.99,
+        date: new Date("2022-10-22"),
+    },
+    {
+        id: "e10",
+        description: "A PAIR OF SOME PAIRS",
+        amount: 69.99,
+        date: new Date("2022-10-22"),
+    },
 ];
 function ExpensesOutput({ expenses, expensesPeriod }) {
     return (
@@ -63,7 +81,9 @@ export default ExpensesOutput;
 
 const styles = StyleSheet.create({
     container: {
-        padding: 24,
+        paddingHorizontal: 24,
+        paddingBottom: 0,
+        paddingTop: 24,
         backgroundColor: GlobalStyles.colors.primary700,
         flex: 1,
     },
