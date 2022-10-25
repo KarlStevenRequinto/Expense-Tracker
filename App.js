e51ed0990d2cdd51a632199ce36d2f8912ffe3be
KarlStevenRequinto <lerdrobott@gmail.com>
2022-10-25 14:10:29 +0800

    Navigating Programmatically Between Screens

--- App.js
+++ App.js
@@ -15,7 +15,7 @@ const BottomTabs = createBottomTabNavigator();
 function ExpensesOverview() {
     return (
         <BottomTabs.Navigator
-            screenOptions={{
+            screenOptions={({ navigation }) => ({
                 headerStyle: {
                     backgroundColor: GlobalStyles.colors.primary500,
                 },
@@ -30,11 +30,13 @@ function ExpensesOverview() {
                             icon="add"
                             size={24}
                             color={tintColor}
-                            onPress={() => {}}
+                            onPress={() => {
+                                navigation.navigate("ManageExpense");
+                            }}
                         />
                     );
                 },
-            }}
+            })}
         >
             <BottomTabs.Screen
                 name="RecentExpenses"
